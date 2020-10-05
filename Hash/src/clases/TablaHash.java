bd89cf0b816d51eb78118fabbd0f6b3e0dd513e3
Miguel Matul <miguemat4@gmail.com>
2020-10-05 00:00:52 -0600

    Clases para la estructura de datos

--- Hash/src/clases/TablaHash.java
+++ Hash/src/clases/TablaHash.java
@@ -1,3 +1,5 @@
+package clases;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +11,34 @@
  * @author migu_
  */
 public class TablaHash {
+    private Slot[] tabla;
+    int maximo = 0;
     
+    public TablaHash(int tope){
+        maximo = tope;
+        tabla = new Slot[maximo];
+    }
+    
+    public int hash(int valor){
+        return ((valor + 1) % maximo);
+    }
+    
+    public void insertar(int valor){
+        int llave = this.hash(valor);
+        tabla[llave] = new Slot();
+        tabla[llave].llenar(valor);
+    }
+    
+    public int buscar(int valor){
+        int llave = this.hash(valor);
+        if (tabla[llave].libre)
+            llave = -1;
+        return llave;
+    }
+    
+    public void eliminar(int valor){
+        int llave = buscar(valor);
+        if (llave != -1)
+            tabla[llave].vaciar();
+    }
 }
